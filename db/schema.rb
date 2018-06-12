6c3eaaf886b199e831678149dad92a063b577de8
Vinh <vinhkyha1235@outlook.com.vn>
2018-06-12 14:28:39 +0700

    vinhr

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_024821) do
+ActiveRecord::Schema.define(version: 2018_06_12_024348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "sex"
+    t.string "styled"
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "name"
