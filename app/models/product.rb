2aebcd3600093dba221367dd1ab3680eec24ab98
Vinh <vinhkyha1235@outlook.com.vn>
2018-06-13 08:40:21 +0700

    vinh2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   enum sex: [:Nam, :Nu]
+  has_attached_file :image, styles: {medium: "10000x10000>", thumb: "10000x10000>"}, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_12_083819) do
+ActiveRecord::Schema.define(version: 2018_06_12_102844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2018_06_12_083819) do
     t.datetime "updated_at", null: false
     t.string "sex"
     t.string "styled"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "tests", force: :cascade do |t|
